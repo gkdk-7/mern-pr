7befacca41bb0a35d2c37ac21b40a3798db0d7a9
gkdk-7 <girishkumarpodugu@gmail.com>
2026-05-03 10:06:20 +0530

    this is a commit message upto todo from d5

---
+++ js/external.js
@@ -0,0 +1,59 @@
+console.log("this is external js file");
+document.write("sample output display using document document object");
+
+num = "java";
+console.log(num);
+num = "java";
+num = `addition of 
+10 and 20`;
+console.log(num);
+num = 50;
+console.log(num);
+
+num = 50;
+console.log(num);
+
+class sampleExample {
+  display() {
+    console.log("this is a non static example");
+  }
+
+  Test() {
+    console.log("this is a another test  example");
+  }
+}
+
+s1 = new sampleExample();
+s1.display();
+s1.Test();
+
+num=331;
+result=num%2==0?"even number":"odd number"
+console
+
+
+a=10;
+b='10';
+c=null
+d=Symbol(10);
+e=123456789n;
+let f;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+
+
+let z=10;
+function test(){
+  let s=10;
+}
+ var arr=10;
+ arr=20;
+ var arr="nodeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+ console.log(arr);
+ 
+
+ 
